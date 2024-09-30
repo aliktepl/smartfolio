b628ef4d54d82c5e9428257d665f59cc890a1361
yoavShargay <yoavshra@gmail.com>
2024-09-30 15:20:20 +0300

    adding the basic express server, and creating router tht leads to the data

--- webclient/src/Main/wallet/Wallet.tsx
+++ webclient/src/Main/wallet/Wallet.tsx
@@ -1,10 +1,24 @@
 import {Coin, columns } from "./columns"
+import { useEffect, useState } from "react";
 import { DataTable } from "./data-table"
 import {useRouteLoaderData} from "react-router-dom";
 import {ModeToggle} from "@/components/mode-toggle.tsx";
 
+
+
+
 export default function Wallet() {
-    const data = useRouteLoaderData("root") as Coin[];
+    //const data = useRouteLoaderData("root") as Coin[];
+
+    const [data, setData] = useState<Coin[]>([]);
+
+    // Fetch wallet data from the backend
+    useEffect(() => {
+      fetch("http://localhost:3000/Users/wallet")
+        .then((response) => response.json())
+        .then((walletData) => setData(walletData))
+        .catch((error) => console.error("Error fetching wallet data:", error));
+    }, []);
 
     return (
         <div className="container mx-auto py-10">
